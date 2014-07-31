977dcd441fb15398793bbc2c578629a95970c7cc
Yuxi <yuxi.sun@amlogic.com>
2014-07-31 15:36:54 +0800

    PD #95261: Directly convert mjpeg to NV21 when preview format is NV21
    Don't using ConverToI420 function anymore, use ConvetToNV21 at first,
    if fail, then try to decode mjpeg with ConvertTo420 function, doing
    this save the memcpy time cost and release cpu loading.
    
    Change-Id: I73b725ea3178387e85ad81179bf136a971401264
    Signed-off-by: Yuxi <yuxi.sun@amlogic.com>

--- V4LCameraAdapter/V4LCameraAdapter.cpp
+++ V4LCameraAdapter/V4LCameraAdapter.cpp
@@ -1826,42 +1826,47 @@ int V4LCameraAdapter::previewThread()
 				return -1;
 			}
 		} else if (CameraFrame::PIXEL_FMT_NV21 == mPixelFormat) {
-			uint8_t *vBuffer = new uint8_t[width * height / 4];
-			if (vBuffer == NULL)
-				CAMHAL_LOGIA("alloc temperary v buffer failed\n");
-			uint8_t *uBuffer = new uint8_t[width * height / 4];
-			if (uBuffer == NULL)
-				CAMHAL_LOGIA("alloc temperary u buffer failed\n");
+			if(ConvertMjpegToNV21(src, mVideoInfo->buf.bytesused, dest, width, dest + width * height, (width + 1) / 2,
+						width, height, width, height, libyuv::FOURCC_MJPG) != 0) {
+				uint8_t *vBuffer = new uint8_t[width * height / 4];
+				if (vBuffer == NULL)
+					CAMHAL_LOGIA("alloc temperary v buffer failed\n");
+				uint8_t *uBuffer = new uint8_t[width * height / 4];
+				if (uBuffer == NULL)
+					CAMHAL_LOGIA("alloc temperary u buffer failed\n");
 
-			if(ConvertToI420(src, mVideoInfo->buf.bytesused, dest, width, uBuffer, (width + 1) / 2,
-						vBuffer, (width + 1) / 2, 0, 0, width, height,
-						width, height, libyuv::kRotate0, libyuv::FOURCC_MJPG) != 0) {
-				fillThisBuffer((uint8_t*) mPreviewBufs.keyAt(mPreviewIdxs.valueFor(index)), CameraFrame::PREVIEW_FRAME_SYNC);
-				CAMHAL_LOGEB("jpeg decode failed,src:%02x %02x %02x %02x",src[0], src[1], src[2], src[3]);
+				if(ConvertToI420(src, mVideoInfo->buf.bytesused, dest, width, uBuffer, (width + 1) / 2,
+							vBuffer, (width + 1) / 2, 0, 0, width, height,
+							width, height, libyuv::kRotate0, libyuv::FOURCC_MJPG) != 0) {
+					fillThisBuffer((uint8_t*) mPreviewBufs.keyAt(mPreviewIdxs.valueFor(index)), CameraFrame::PREVIEW_FRAME_SYNC);
+					CAMHAL_LOGEB("jpeg decode failed,src:%02x %02x %02x %02x",src[0], src[1], src[2], src[3]);
 
 #ifdef AMLOGIC_USB_CAMERA_SUPPORT
-				mFailedCnt++;
-				gettimeofday(&mEndTime, NULL);
-			        int intreval = (mEndTime.tv_sec - mStartTime.tv_sec) * 1000000 + (mEndTime.tv_usec - mStartTime.tv_usec);
-			        if(intreval > (int)mResetTH){
-					CAMHAL_LOGIA("MJPEG Stream error ! Restart Preview");
-					force_reset_sensor();
-					mFailedCnt = 0;
-					mFirstBuff = true;
-				}
+					mFailedCnt++;
+					gettimeofday(&mEndTime, NULL);
+					int intreval = (mEndTime.tv_sec - mStartTime.tv_sec) * 1000000 + (mEndTime.tv_usec - mStartTime.tv_usec);
+					if(intreval > (int)mResetTH){
+						CAMHAL_LOGIA("MJPEG Stream error ! Restart Preview");
+						force_reset_sensor();
+						mFailedCnt = 0;
+						mFirstBuff = true;
+					}
 #endif
 
-				return -1;
-			}
+					delete vBuffer;
+					delete uBuffer;
+					return -1;
+				}
 
-			uint8_t *pUVBuffer = dest + width * height;
-			for (int i = 0; i < width * height / 4; i++) {
-				*pUVBuffer++ = *(vBuffer + i);
-				*pUVBuffer++ = *(uBuffer + i);
-			}
+				uint8_t *pUVBuffer = dest + width * height;
+				for (int i = 0; i < width * height / 4; i++) {
+					*pUVBuffer++ = *(vBuffer + i);
+					*pUVBuffer++ = *(uBuffer + i);
+				}
 
-			delete vBuffer;
-			delete uBuffer;
+				delete vBuffer;
+				delete uBuffer;
+			}
 		}
 #endif //AMLOGIC_PREVIOUS_MJPEG_DECODE
             mFailedCnt = 0; 
@@ -1901,14 +1906,12 @@ int V4LCameraAdapter::previewThread()
                 } else {
 
                         if ( CameraFrame::PIXEL_FMT_NV21 == mPixelFormat){
-                            //if (frame.mLength != mVideoInfo->buf.length) {
                             if (width%32) {
                                     CAMHAL_LOGDB("frame.mLength =%d, mVideoInfo->buf.length=%d\n",
                                                     frame.mLength,  mVideoInfo->buf.length);
                                     nv21_memcpy_align32 (dest, src, width, height);
                             }
                         }else{
-                            //if (frame.mLength != mVideoInfo->buf.length) {
                             if (width%64) {
                                     CAMHAL_LOGDB("frame.mLength =%d, mVideoInfo->buf.length=%d\n",
                                                     frame.mLength,  mVideoInfo->buf.length);
